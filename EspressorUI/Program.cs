d8be70b2d8efeae11c557bfbcdbb891043eb2802
Ciprian BUZATU <ciprian.buzatu@stud.acs.upb.ro>
2021-07-21 16:02:47 +0300

    Added const

--- EspressorUI/Program.cs
+++ EspressorUI/Program.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using Espressorlibrary;
 
-
 namespace EspressorUI
 {
     
     class Program
     {
+        const string END_MESSAGE = "FINISH";
+        const string MESSAGE = "Write " + END_MESSAGE + " to END programm. Available commands: ";
+        const string REQUEST_COMMAND = "Enter your command: ";
         static void Main(string[] args)
         {
             Espressor espressor = new Espressor();
-            string commandFromLine;
-            string endMessage = "FINISH";
-            string message = "Write "+ endMessage +" to END programm. Available commands: ";
-            string requestCommand = "Enter your command: ";
+            string commandFromLine;            
             State currentState = State.Initial_state;
 
             while (true)
@@ -23,12 +22,12 @@ namespace EspressorUI
                 Console.WriteLine("----------------------------------------\n");
                 Console.WriteLine(espressor);
                 Console.WriteLine("----------------------------------------\n");
-                Console.WriteLine(message);
+                Console.WriteLine(MESSAGE);
                 espressor.PrintAvailableAction(currentState);
                 Console.WriteLine("\n");
-                Console.Write(requestCommand);
+                Console.Write(REQUEST_COMMAND);
                 commandFromLine = Console.ReadLine();
-                if (commandFromLine == endMessage) return;
+                if (commandFromLine == END_MESSAGE) return;
                 if (espressor.IsTheCommandPossible(currentState, commandFromLine))
                 {
                     espressor.ExecuteCommand(commandFromLine, ref currentState);
